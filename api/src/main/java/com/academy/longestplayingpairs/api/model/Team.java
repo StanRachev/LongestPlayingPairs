b7913e7fd2fe5b372a24648d7841574df9410d91
Stanimir Rachev <rachev.stanimir7@gmail.com>
2024-09-09 20:38:06 +0300

    Creating model for Team

---
+++ api/src/main/java/com/academy/longestplayingpairs/api/model/Team.java
@@ -0,0 +1,42 @@
+package com.academy.longestplayingpairs.api.model;
+
+import com.academy.longestplayingpairs.api.model.enums.Group;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "teams", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"id", "name", "manager_full_name"})
+})
+@Getter @Setter @NoArgsConstructor
+public class Team {
+
+    @Id
+    @Column(name = "id")
+    private int id;
+    @NotNull
+    @Column(name = "name")
+    private String name;
+    @NotNull
+    @Column(name = "manager_full_name")
+    private String managerFullName;
+    @NotNull
+    @Column(name = "group_f")
+    private Group groupF;
+
+    @OneToMany(mappedBy = "team")
+    private List<Player> players;
+
+    public Team(int id, String name, String managerFullName, Group groupF, List<Player> players) {
+        this.id = id;
+        this.name = name;
+        this.managerFullName = managerFullName;
+        this.groupF = groupF;
+        this.players = players;
+    }
+}
