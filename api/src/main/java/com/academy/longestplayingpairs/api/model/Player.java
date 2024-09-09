8bee9c30c9b7d564931b1f70caf175830b1c5a1b
Stanimir Rachev <rachev.stanimir7@gmail.com>
2024-09-09 20:37:48 +0300

    Creating model for Player

---
+++ api/src/main/java/com/academy/longestplayingpairs/api/model/Player.java
@@ -0,0 +1,41 @@
+package com.academy.longestplayingpairs.api.model;
+
+import com.academy.longestplayingpairs.api.model.enums.PlayerPosition;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "players", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"id", "full_name"})
+})
+@Getter @Setter @NoArgsConstructor
+public class Player {
+
+    @Id
+    @Column(name = "id")
+    private int id;
+    @NotNull
+    @Column(name = "team_number")
+    private int teamNumber;
+    @NotNull
+    @Column(name = "position")
+    private PlayerPosition position;
+    @NotNull
+    @Column(name = "full_name")
+    String fullName;
+
+    @ManyToOne
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Player(int id, int teamNumber, PlayerPosition position, String fullName, Team team) {
+        this.id = id;
+        this.teamNumber = teamNumber;
+        this.position = position;
+        this.fullName = fullName;
+        this.team = team;
+    }
+}
