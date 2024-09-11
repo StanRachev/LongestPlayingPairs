170542d60267d6ed5a9d7f2c882966cd835ac5d7
Stanimir Rachev <rachev.stanimir7@gmail.com>
2024-09-11 20:32:12 +0300

    Interface for the TeamService

---
+++ api/src/main/java/com/academy/longestplayingpairs/api/service/interfaces/TeamServiceI.java
@@ -0,0 +1,10 @@
+package com.academy.longestplayingpairs.api.service.interfaces;
+
+import com.academy.longestplayingpairs.api.model.Team;
+
+public interface TeamServiceI {
+
+    public String createTeam(Team team);
+    public String editTeam(int id, Team team);
+    public void removeTeam(int id);
+}
