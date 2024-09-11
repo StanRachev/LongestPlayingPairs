b9689212545989c65ce592b9467678875de001ad
Stanimir Rachev <rachev.stanimir7@gmail.com>
2024-09-11 20:26:13 +0300

    validates the new team added

---
+++ api/src/main/java/com/academy/longestplayingpairs/api/model/TeamValidator.java
@@ -0,0 +1,16 @@
+package com.academy.longestplayingpairs.api.model;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TeamValidator {
+
+    public static boolean isValid(Team team) {
+        String toCheck = team.getName() + ", " + team.getManagerFullName();
+
+        Pattern pattern = Pattern.compile("\\s*(?<name>([A-Z][a-z]+\\s+){0,2}[A-Z][a-z]+)\\s*[,;-]\\s*(?<manager>(([\\p{L}]+\\s+){0,5}[\\p{L}]+|[a-z]+)(?: \\([a-z]+\\))?)\\s*");
+        Matcher matcher = pattern.matcher(toCheck);
+
+        return matcher.matches();
+    }
+}
